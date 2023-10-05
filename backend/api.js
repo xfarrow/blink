e2bcaf66c8b44c773e09e23a60b09669c2bcadd3
xfarrow <ale99napoli@icloud.com>
2023-10-05 12:20:00 +0200

    Update

--- backend/api.js
+++ backend/api.js
@@ -35,6 +35,10 @@ app.get('/api/items', (req, res) => {
 });
 
 // POST - Register an account
+
+// (req, res) => { ... } is a callback which usually indicates that the
+// execution of the code contained between brackets will continue
+// asynchronously.
 app.post('/api/register', (req, res) => {
   const userData = req.body;
   
@@ -43,8 +47,8 @@ app.post('/api/register', (req, res) => {
     return res.status(400).json("Invalid request");
   }
 
-  // The callback denoted by the arrow function is executed asynchronously
-  // from the rest of the code outside of the hash() function
+  // The callback denoted by the arrow function is executed
+  // when hash() has finished its execution.
   bcrypt.hash(userData.password, 10, (err, hashedPassword) => {
     
     if (err) {
@@ -93,6 +97,55 @@ app.post('/api/register', (req, res) => {
     }
 
   });
+});
+
+app.post('/api/registerv2', (req, res) => {
+  const userData = req.body;
+  
+  // Ensure that the required fields are present before proceeding
+  if (!userData.display_name || !userData.email || !userData.password) {
+    return res.status(400).json("Invalid request");
+  }
+
+  bcrypt.hash(userData.password, 10)
+    .then( hashedPassword => {
+      // Generate activation link token
+      const activationLink = crypto.randomBytes(16).toString('hex');
+          // Acquire a connection from the pool
+          pool.connect()
+          .then(async (client) => {
+            // SQL query with placeholders for parameters
+            const insertQuery = `
+            INSERT INTO "User" (display_name, date_of_birth, place_of_living, is_looking_for_job, email, password)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING *`; // Return the inserted row
+      
+          try {
+              try {
+                const result = await client.query(insertQuery, [
+                  userData.display_name,
+                  userData.date_of_birth,
+                  userData.place_of_living,
+                  userData.is_looking_for_job,
+                  userData.email,
+                  hashedPassword
+                ]);
+                // Respond with the inserted user data
+                res.status(200).json(result.rows[0]);
+              } catch (error) {
+                console.error('Error inserting data:', error);
+                res.status(500).json("Internal server error");
+              }
+            } finally {
+              // Release the connection back to the pool
+              client.release();
+            }
+        })
+        .catch((error) => {
+          console.error('Error acquiring a connection from the pool:', error);
+          res.status(500).json("Internal server error");
+        });
+    });
 });
 
 // Start the server
