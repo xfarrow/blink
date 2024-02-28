9244923ae3aac412d74f53e00dcf2f330f11701c
xfarrow <ale99napoli@icloud.com>
2024-02-28 15:51:21 +0100

    login and my profile

---
+++ frontend/vanilla/js/utils.js
@@ -0,0 +1,11 @@
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return decodeURIComponent(cookieValue);
+        }
+    }
+    return null;
+}
