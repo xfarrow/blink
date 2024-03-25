468e70c22bba160bce4cef8fb6e9515a17a5463a
xfarrow <ale99napoli@icloud.com>
2024-03-25 17:44:06 +0100

    Update 10-create_request_reset_password.sql

--- backend/sql/10-create_request_reset_password.sql
+++ backend/sql/10-create_request_reset_password.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS public."RequestResetPassword"
     id serial,
     email character varying(128) NOT NULL,
     secret character varying NOT NULL,
-    time_of_request timestamp with time zone NOT NULL DEFAULT now(),
+    time_of_request timestamp without time zone NOT NULL DEFAULT now(),
     CONSTRAINT "RequestResetPassword_pkey" PRIMARY KEY (id)
 )
 
