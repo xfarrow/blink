e1bb44ca1807ec96ddad303d87d98af04b6eb03d
xfarrow <ale99napoli@icloud.com>
2024-03-25 10:07:54 +0100

    New register page

--- frontend/vanilla/js/utils.js
+++ frontend/vanilla/js/utils.js
@@ -10,6 +10,20 @@ function getCookie(name) {
     return null;
 }
 
+/**
+ * Validates an e-mail using a RegExpression
+ *
+ * @param {*} email
+ * @returns 
+ */
+function validateEmail(email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+}
+
 function callbackErrors(errors, func) {
     errors.forEach(error => func(error.msg));
 }
@@ -19,4 +33,11 @@ function createHeaders(token) {
         "Content-type": "application/json; charset=UTF-8",
         "Authorization": `Bearer ${token}`
     }
+}
+
+function clearInputFields() {
+    var inputs = document.querySelectorAll('input');
+    inputs.forEach(function(input) {
+        input.value = '';
+    });
 }
