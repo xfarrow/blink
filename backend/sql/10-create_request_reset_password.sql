2abf27a7c768371683c62d0d087ccd8f781e8ed3
xfarrow <ale99napoli@icloud.com>
2024-03-25 17:43:13 +0100

    add .sql and API usage example

---
+++ backend/sql/10-create_request_reset_password.sql
@@ -0,0 +1,17 @@
+-- Table: public.RequestResetPassword
+
+-- DROP TABLE IF EXISTS public."RequestResetPassword";
+
+CREATE TABLE IF NOT EXISTS public."RequestResetPassword"
+(
+    id serial,
+    email character varying(128) NOT NULL,
+    secret character varying NOT NULL,
+    time_of_request timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT "RequestResetPassword_pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."RequestResetPassword"
+    OWNER to postgres;
